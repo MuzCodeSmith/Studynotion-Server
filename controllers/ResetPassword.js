e221e50a600518a4a2547b6c4d0df2734f4ec81f
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-02-02 19:39:47 +0530

    add reset Password controller

--- controllers/ResetPassword.js
+++ controllers/ResetPassword.js
@@ -1,5 +1,9 @@
+// models
+const { findByIdAndUpdate } = require("../models/Profile");
 const User = require("../models/User");
+// packages
 const mailSender = require("../utils/mailsender") 
+const bcrypt = require('bcrypt');
 // resetPasswordToken
 
 exports.resetPasswordToken = async (req,res)=>{
@@ -37,6 +41,57 @@ exports.resetPasswordToken = async (req,res)=>{
         return res.json({
             success:false,
             message:"Something went wrong while reset password mail"
+        })
+    }
+}
+
+// reset Password
+exports.resetPassword = async (req, res) =>{
+    try {
+        // extracting data from request
+        const {password, confirmPassword, token} = req.body;
+        // validating data
+        if(password !== confirmPassword) {
+            return res.json(401).json({
+                success:false,
+                message:"password not matching"
+            })
+        }
+        // fetching user details using token
+        const userDetails = await User.findOne({token:token});
+        if(!userDetails){
+            return res.json(401).json({
+                success:false,
+                message:"token is invalid"
+            })
+        }
+        // check token expiry
+        if(userDetails.resetPasswordExpires < Date.now()){
+            return res.json(401).json({
+                success:false,
+                message:"token is expired "
+            })
+        }
+        // hashing password
+        const hashedPassword = await bcrypt.hash(password,10)
+
+        // updating userdetails
+        const updatedDeatails = await User.findOneAndUpdate(
+            {token:token},
+            {password:hashedPassword},
+            {new:true}
+        );
+
+        res.status(200).status({
+            success:true,
+            message:"Password resetted successfully"
+        })
+        
+    } catch (error) {
+        console.error(error)
+        return res.json({
+            success:false,
+            message:"Something went wrong while resetting password"
         })
     }
 }
