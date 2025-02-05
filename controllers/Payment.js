bfb3aed6086ab339cdca0a7ac426b268cdf0fc6d
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-02-05 22:58:06 +0530

    add payment controller

---
+++ controllers/Payment.js
@@ -0,0 +1,4 @@
+const {instance} = require('../config/razorpay')
+const User = require('../models/User');
+const Course = require('../models/Course');
+const mailSender = require('../utils/mailsender');
