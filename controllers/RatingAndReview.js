b3a72ffa1a7cf6ae09cf867cfa7e7c403f9511bf
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-02-08 20:24:24 +0530

    add getAverageRating controller

--- controllers/RatingAndReview.js
+++ controllers/RatingAndReview.js
@@ -1,6 +1,7 @@
 const RatingAndReview = require('../models/RatingAndReview');
 const Course = require('../models/Course');
 const User = require('../models/User');
+const { default: mongoose } = require('mongoose');
 
 exports.createRating = async (req, res) => {
     try {
@@ -43,8 +44,8 @@ exports.createRating = async (req, res) => {
         console.log(updatedCourse)
         return res.status(200).json({
             success: true,
-            message: `review and rating added successfully`
-            ratingReview,
+            message: `review and rating added successfully`,
+            ratingReview
         })
 
     } catch (error) {
@@ -52,5 +53,45 @@ exports.createRating = async (req, res) => {
             success: false,
             message: 'error while rating: ' + error.message
         })
+    }
+}
+
+exports.getAverageRating = async (req,res) => {
+    try {
+        const courseId = req.body.courseId;
+
+        // calculate rating
+        const result = await RatingAndReview.aggregate([
+            {
+                $match:{
+                    course:new mongoose.Types.ObjectId(courseId),
+                },
+            },
+            {
+                $group:{
+                    _id:null,
+                    averageRating:{$avg:"$rating"}
+                }
+            }
+        ])
+
+        if(result.length > 0){
+            return res.status(200).json({
+                success:true,
+                averageRating:result[0].averageRating
+            })  
+        }
+        // if no rating /review exists
+        return res.status(400).json({
+            success:false,
+            message:"no ratings given till now",
+            averageRating:0
+
+        })  
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            message:`error occured while calculating average of rating: ${error.message}`
+        })     
     }
 }
